12ef3829d8b68efa67e29f3b995d19c3f387bb6e
Hanno Schlichting <hanno@hannosch.eu>
2006-05-25 16:16:54 +0000

    Added easy_install/egg infrastructure generated by zopeskel.
    svn path=/plone.i18n/trunk/; revision=9895

---
+++ plone/i18n/README.txt
@@ -0,0 +1,8 @@
+plone.i18n Package Readme
+=========================
+
+Overview
+--------
+
+Advanced i18n/l10n features for Plone.
+
---
+++ setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+import sys, os
+
+version = '0.1'
+
+setup(name='plone.i18n',
+      version=version,
+      description="Advanced i18n/l10n features",
+      long_description="""\
+""",
+      classifiers=[], # Get strings from http://www.python.org/pypi?%3Aaction=list_classifiers
+      keywords='i18n l10n Plone',
+      author='Plone Foundation',
+      author_email='plone-developers@lists.sourceforge.net',
+      url='http://svn.plone.org/svn/plone/plone.i18n',
+      license='GPL',
+      packages=find_packages(exclude=['ez_setup']),
+      namespace_packages=['plone'],
+      include_package_data=True,
+      zip_safe=False,
+      install_requires=[
+          # -*- Extra requirements: -*-
+      ],
+      entry_points="""
+      # -*- Entry points: -*-
+      """,
+      )
+      
