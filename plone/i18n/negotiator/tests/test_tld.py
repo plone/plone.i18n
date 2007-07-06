a43076ca4b321df29ecce4adde5f6e9deac51167
Hanno Schlichting <hanno@hannosch.eu>
2007-07-06 16:42:07 +0000

    Added cctld based negotiator plugin
    svn path=/plone.i18n/branches/udg-sprint/; revision=15721

---
+++ plone/i18n/negotiator/tests/test_tld.py
@@ -0,0 +1,71 @@
+# -*- coding: UTF-8 -*-
+"""
+    Negotiator tests.
+"""
+
+import unittest
+
+import zope.component
+from zope.component.testing import setUp, tearDown
+from zope.configuration.xmlconfig import XMLConfig
+
+from zope.testing import doctest
+from zope.testing.doctestunit import DocTestSuite
+
+
+class TestRequest(dict):
+
+    def __init__(self, host):
+        self["HTTP_HOST"] = host
+
+
+def configurationSetUp(self):
+    setUp()
+    XMLConfig('meta.zcml', zope.component)()
+
+
+def testPathNegotiator():
+    """
+      >>> from plone.i18n.negotiator.negotiator import Negotiator
+      >>> negotiator = Negotiator()
+      >>> from plone.i18n.negotiator.tld import TldLanguage
+      >>> negotiator[0] = TldLanguage
+
+      >>> from plone.i18n.locales.interfaces import ICcTLDInformation
+      >>> from plone.i18n.locales.interfaces import ILanguageAvailability
+      >>> from plone.i18n.locales.cctld import CcTLDInformation
+      >>> from plone.i18n.locales.languages import LanguageAvailability
+
+      >>> from zope.component import getSiteManager
+      >>> lang_avail = LanguageAvailability()
+      >>> cctld = CcTLDInformation()
+
+      >>> sm = getSiteManager()
+      >>> sm.registerUtility(lang_avail, provided=ILanguageAvailability)
+      >>> sm.registerUtility(cctld, provided=ICcTLDInformation)
+
+      >>> data = [
+      ...    (('de', 'en'),  'www.plone.org'),
+      ...    (('en', 'fi'), 'www.plone.fi'),
+      ...    (('ca', 'pt'), 'www.plone.pt'),
+      ...    (('ca', 'pt'), 'www.plone.pt:8081'),
+      ... ]
+
+      >>> for langs, host in data:
+      ...    request = TestRequest(host)
+      ...    negotiator.getLanguage(langs, request)
+      'fi'
+      'pt'
+      'pt'
+    """
+
+
+def test_suite():
+    return unittest.TestSuite((
+        DocTestSuite(setUp=configurationSetUp,
+                     tearDown=tearDown,
+                     optionflags=doctest.ELLIPSIS | doctest.NORMALIZE_WHITESPACE),
+        ))
+
+if __name__ == '__main__':
+    unittest.main(defaultTest="test_suite")
---
+++ plone/i18n/negotiator/tld.py
@@ -0,0 +1,33 @@
+from zope.component import queryUtility
+from zope.i18n.interfaces import ILanguageAvailability
+from zope.i18n.interfaces import IUserPreferredLanguages
+from zope.interface import implements
+
+from plone.i18n.locales.interfaces import ICcTLDInformation
+
+
+class TldLanguage(object):
+
+    implements(IUserPreferredLanguages)
+
+    def __init__(self, request):
+        self.request = request
+
+    def getPreferredLanguages(self):
+        """Return a sequence of user preferred languages."""
+        langs = []
+
+        host = self.request.get('HTTP_HOST', None)
+        util = queryUtility(ICcTLDInformation)
+        if host is None or util is None:
+            return None
+
+        tld = host.split(":")[0].lower().split(".")[-1]
+        wanted = util.getTLDs().get(tld, [])
+
+        available = queryUtility(ILanguageAvailability)
+        if available is not None:
+            langs = [str(lang) for lang in available.getAvailableLanguages()]
+
+        langs = [lang for lang in wanted if lang in langs]
+        return langs and langs or None
