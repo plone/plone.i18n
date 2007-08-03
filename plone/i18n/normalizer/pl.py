414c745698ff959522c02cb56a77f4e8ffca892a
Hanno Schlichting <hanno@hannosch.eu>
2007-08-03 07:42:19 +0000

    Added Polish normalizer which normalizes 'l with stroke' to l. This fixes #6845.
    svn path=/plone.i18n/trunk/; revision=16283

---
+++ plone/i18n/normalizer/pl.py
@@ -0,0 +1,31 @@
+# -*- coding: UTF-8 -*-
+
+from plone.i18n.normalizer.interfaces import INormalizer
+from zope.interface import implements
+from plone.i18n.normalizer.base import mapUnicode
+
+# Polish character mapping
+mapping = {
+    322 : 'l',
+}
+
+class Normalizer(object):
+    """
+    This normalizer can normalize any unicode string and returns a version
+    that only contains of ASCII characters.
+
+    Let's make sure that this implementation actually fulfills the API.
+
+      >>> from zope.interface.verify import verifyClass
+      >>> verifyClass(INormalizer, Normalizer)
+      True
+    """
+    implements(INormalizer)
+
+    def normalize(self, text, locale=None):
+        """
+        Returns a normalized text. text has to be a unicode string.
+        """
+        return mapUnicode(text, mapping=mapping)
+
+normalizer = Normalizer()
