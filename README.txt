1a915058a635ea2c4fa120af54589da1422d4be3
Hanno Schlichting <hanno@hannosch.eu>
2008-08-18 18:38:18 +0000

    Prepare release
    svn path=/plone.i18n/trunk/; revision=22247

--- README.txt
+++ README.txt
@@ -1,8 +1,5 @@
-plone.i18n Package Readme
-=========================
-
 Overview
---------
+========
 
 Advanced i18n/l10n features useful in a CMS environment.
 
--- setup.py
+++ setup.py
@@ -6,17 +6,16 @@ version = '1.0.6'
 setup(name='plone.i18n',
       version=version,
       description="Advanced i18n/l10n features",
-      long_description="""\
-""",
+      long_description=open("README.txt").read() + "\n" +
+                       open(os.path.join("docs", "HISTORY.txt")).read(),
       classifiers=[
         'Framework :: Plone',
         'Framework :: Zope2',
-        'Framework :: Zope3'
       ],
       keywords='i18n l10n Plone',
       author='Plone Foundation',
       author_email='plone-developers@lists.sourceforge.net',
-      url='http://svn.plone.org/svn/plone/plone.i18n',
+      url='http://pypi.python.org/pypi/plone.i18n',
       license='GPL',
       packages=find_packages(exclude=['ez_setup']),
       namespace_packages=['plone'],
