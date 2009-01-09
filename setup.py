ebbe2a797815d5a73d51c8fd5f685ba81ce4746c
Hanno Schlichting <hanno@hannosch.eu>
2009-01-09 13:08:43 +0000

    Reformat setup.py
    svn path=/plone.i18n/trunk/; revision=24141

--- setup.py
+++ setup.py
@@ -3,36 +3,41 @@ import sys, os
 
 version = '1.0.8'
 
-setup(name='plone.i18n',
-      version=version,
-      description="Advanced i18n/l10n features",
-      long_description=open("README.txt").read() + "\n" +
-                       open(os.path.join("docs", "HISTORY.txt")).read(),
-      classifiers=[
+setup(
+    name='plone.i18n',
+    version=version,
+    description="Advanced i18n/l10n features",
+    long_description=open("README.txt").read() + "\n" +
+                     open(os.path.join("docs", "HISTORY.txt")).read(),
+    classifiers=[
         'Framework :: Plone',
         'Framework :: Zope2',
-      ],
-      keywords='i18n l10n Plone',
-      author='Plone Foundation',
-      author_email='plone-developers@lists.sourceforge.net',
-      url='http://pypi.python.org/pypi/plone.i18n',
-      license='GPL',
-      packages=find_packages(exclude=['ez_setup']),
-      namespace_packages=['plone'],
-      include_package_data=True,
-      zip_safe=False,
-      install_requires=[
+    ],
+    keywords='i18n l10n Plone',
+    author='Plone Foundation',
+    author_email='plone-developers@lists.sourceforge.net',
+    url='http://pypi.python.org/pypi/plone.i18n',
+    license='GPL',
+    packages=find_packages(exclude=['ez_setup']),
+    namespace_packages=['plone'],
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=[
         'setuptools',
+        'zope.i18n',
         'zope.interface',
-      ],
-      extras_require=dict(
-          test = [ 'zope.app.publisher',
-                   'zope.component [zcml]', 
-                   'zope.configuration',
-                   'zope.testing',
-                 ],
-          negotioator = [ 'zope.component',
-                          'ZODB3',
-                        ],
-      ),
-      )
+    ],
+    extras_require=dict(
+        test=[
+            'zope.component [zcml]',
+            'zope.configuration',
+            'zope.publisher',
+            'zope.testing',
+            'zope.app.publisher',
+        ],
+        negotiator = [
+            'zope.component',
+            'ZODB3',
+        ],
+    ),
+)
