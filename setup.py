ec2e054ad543651a15775c188e574a7a29e6264a
Maurits van Rees <maurits@vanrees.org>
2016-02-09 21:55:05 +0100

    Fixed classifiers in setup.py

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ setup(
     classifiers=[
         "Environment :: Web Environment",
         "Framework :: Plone",
-        "Framework :: Plone : 5.0",
+        "Framework :: Plone :: 5.0",
         "Framework :: Zope2",
         "License :: OSI Approved :: GNU General Public License (GPL)",
         "Operating System :: OS Independent",
