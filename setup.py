e57a0520462ed6a2972118b5ab1cebe2198e20ac
Maurits van Rees <maurits@vanrees.org>
2018-10-01 15:39:16 +0200

    Fixed PyPI links in setup.py.
    [ci skip]

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ setup(
     keywords='i18n l10n Plone',
     author='Plone Foundation',
     author_email='plone-developers@lists.sourceforge.net',
-    url='https://pypi.python.org/pypi/plone.i18n',
+    url='https://pypi.org/project/plone.i18n',
     license='GPL version 2',
     packages=find_packages(exclude=['ez_setup']),
     namespace_packages=['plone'],
