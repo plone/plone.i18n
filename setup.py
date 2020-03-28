cef751eb1f373aa8db8b7613b39b1efab9705973
Maurits van Rees <maurits@vanrees.org>
2020-03-28 01:26:50 +0100

    Added Python 3.8 to classifiers in setup.py.
    [ci skip]

--- setup.py
+++ setup.py
@@ -22,6 +22,7 @@ setup(
         "Programming Language :: Python :: 2.7",
         "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
     ],
     keywords='i18n l10n Plone',
     author='Plone Foundation',
